d56cfe34d8ab9674b25865c2815910b0f6ea7546
anders-schneider <aschn@seas.upenn.edu>
2015-09-05 17:27:05 -0400

    Adding some of sql insert files

---
+++ Database Inserts/auth_users.sql
@@ -0,0 +1,4 @@
+insert into auth_user (password, username, first_name, last_name, email) VALUES ("kpbho", "rsmith", "Ryan", "Smith", "rysmit@seas.upenn.edu");
+insert into auth_user (password, username, first_name, last_name, email) VALUES ("kpbho", "aschneider", "Andy", "Schneider", "aschn@seas.upenn.edu");
+insert into auth_user (password, username, first_name, last_name, email) VALUES ("kpbho", "afiuk", "Alex", "Fiuk", "afiuk@seas.upenn.edu");
+insert into auth_user (password, username, first_name, last_name, email) VALUES ("kpbho", "qhe", "Qi", "He", "heqi@seas.upenn.edu");
---
+++ Database Inserts/datetimes.sql
@@ -0,0 +1,9 @@
+insert into block_party_datetime (date, time_of_day) values ('2015-09-04', "Morning");
+insert into block_party_datetime (date, time_of_day) values ('2015-09-04', "Afternoon");
+insert into block_party_datetime (date, time_of_day) values ('2015-09-04', "Evening");
+insert into block_party_datetime (date, time_of_day) values ('2015-09-05', "Morning");
+insert into block_party_datetime (date, time_of_day) values ('2015-09-05', "Afternoon");
+insert into block_party_datetime (date, time_of_day) values ('2015-09-05', "Evening");
+insert into block_party_datetime (date, time_of_day) values ('2015-09-06', "Morning");
+insert into block_party_datetime (date, time_of_day) values ('2015-09-06', "Afternoon");
+insert into block_party_datetime (date, time_of_day) values ('2015-09-06', "Evening");
