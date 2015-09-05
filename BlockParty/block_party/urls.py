5bd6bd0ed57aad85c2376f791e6f94fc49e236f4
RyCSmith <rycsmith@gmail.com>
2015-09-05 13:33:47 -0400

    Added url routing for block_party app.

---
+++ BlockParty/block_party/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+	#this will process and route any url requets sent to the polls app. 
+	#by the time a request gets here, the pattern that sent it here has been removed
+	#it will then route the processing of that request to a function in the views.py file for this app
+    
+    # ex: /
+    url(r'^$', views.index, name='index'),
+    # ex: /signup
+    url(r'^signup/$', views.signup, name='signup'),
+    # ex: /create_event
+    url(r'^create_event/$', views.create_event, name='create_event'),
+    # ex: /events
+    url(r'^events/$', views.events, name='events'),
+    # ex: /events/<id>
+    url(r'^events/(?P<event_id>[0-9]+)/$', views.event_details, name='event_details'),
+    # ex: /profile
+    url(r'^profile/$', views.profile, name='profile'),
+]
