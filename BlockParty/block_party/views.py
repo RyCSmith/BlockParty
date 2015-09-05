88226997bdc54ec20fc4745c1575cebcff100c6b
RyCSmith <rycsmith@gmail.com>
2015-09-05 13:37:12 -0400

    Created preliminary views functions

--- BlockParty/block_party/views.py
+++ BlockParty/block_party/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.template import RequestContext, loader
+from django.http import HttpResponse, HttpResponseRedirect
+#from .models import 
+from django.core.urlresolvers import reverse
 
-# Create your views here.
+def index(request):
+	return HttpResponse("index")
+
+def signup(request):
+	return HttpResponse("signup")
+
+def events(request):
+	return HttpResponse("events")
+
+def event_details(request, event_id):
+	return HttpResponse("event details: " + str(event_id))
+
+def create_event(request):
+	return HttpResponse("create_event")
+
+def profile(request):
+	return HttpResponse("profile")
+
