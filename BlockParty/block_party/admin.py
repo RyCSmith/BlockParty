f63d49058ec5bf2dda38367a234a855c2c3425a7
RyCSmith <rycsmith@gmail.com>
2015-09-05 13:39:16 -0400

    Added some models to admin site.

--- BlockParty/block_party/admin.py
+++ BlockParty/block_party/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CorporateProfile, IndividualProfile, Event
 
-# Register your models here.
+class CorporateProfileAdmin(admin.ModelAdmin):
+	fields = ['user', 'email', 'phone_num']
+
+admin.site.register(CorporateProfile, CorporateProfileAdmin)
