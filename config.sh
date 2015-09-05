1717bb501dfb616f58162f92f4d7d837d9b6ebb5
RyCSmith <rycsmith@gmail.com>
2015-09-04 23:09:07 -0400

    Update Vagrant configuration

--- config.sh
+++ config.sh
@@ -3,3 +3,5 @@ apt-get -qqy install python-flask python-sqlalchemy
 apt-get -qqy install python-pip
 pip install bleach
 pip install django
+apt-get -y install python-mysqldb
+apt-get -y install sqlite3
