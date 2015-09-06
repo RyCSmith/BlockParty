ec023fe7d8cda7425c8310a332dda1b98b10865b
anders-schneider <aschn@seas.upenn.edu>
2015-09-05 21:23:23 -0400

    Added all test data

---
+++ Database Inserts/eventcreationcorporates.sql
@@ -0,0 +1,3 @@
+insert into block_party_eventcreationcorporate (creator_id, event_id) values (1, 1);
+insert into block_party_eventcreationcorporate (creator_id, event_id) values (1, 2);
+insert into block_party_eventcreationcorporate (creator_id, event_id) values (1, 3);
